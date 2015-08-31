1bc0dd62d95a91d483985d94769a474024601177
Frederic Heem <frederic.heem@gmail.com>
2015-08-31 21:38:54 +0100

    add rabbitmq-pubsub

--- server/test/jobs/testSubscriber.js
+++ server/test/jobs/testSubscriber.js
@@ -2,8 +2,11 @@ var assert = require('assert');
 var _ = require('lodash');
 var Promise = require('bluebird');
 var debug = require('debug');
-var Subscriber = require('../../lib/mq/subscriber.js');
-var Publisher = require('../../lib/mq/publisher');
+var rabbitmq = require('rabbitmq-pubsub')
+var Subscriber = require("rabbitmq-pubsub").Subscriber;
+var Publisher = require("rabbitmq-pubsub").Publisher;
+//var Publisher = require('../../lib/mq/publisher');
+
 var testManager = require('../testManager');
 var app = testManager.app;
 
@@ -24,107 +27,8 @@ describe('PublisherSubscriber', function() {
       publisher.start().delay(1e3).then(publisher.stop).then(done, done);
     });
 
-    it('should start, purge the queue and stop the subscriber', function(done) {
-      subscriber = new Subscriber(app, mqOptions);
-      subscriber.start()
-      .delay(1e3)
-      .then(subscriber.purgeQueue)
-      .then(subscriber.stop)
-      .then(done, done);
-    });
 
-    it('should stop the subscriber without start', function(done) {
-      subscriber = new Subscriber(app, mqOptions);
-      subscriber.stop().then(done, done);
-    });
-
-    it('should start and stop the publisher and subscriber', function(done) {
-      publisher = new Publisher(app, {exchange:"user.new"});
-      subscriber = new Subscriber(app, mqOptions);
-      Promise.all(
-        [
-          publisher.start(),
-          subscriber.start()
-         ])
-      .delay(1e3)
-      .then(function() {
-        return Promise.all(
-          [
-            publisher.stop(),
-            subscriber.stop()
-           ]);
-      })
-      .then(function() {
-
-      })
-      .then(done, done);
-    });
   });
 
-  describe('Subscriber', function() {
-    before(function(done) {
-      debug("publisher.start()");
-      publisher = new Publisher(app, {exchange:"user.new"});
-      publisher.start().then(done, done);
-    });
 
-    after(function(done) {
-      debug("publisher.stop()");
-      publisher.stop().then(done, done);
-    });
-
-    it('should receive the published message', function(done) {
-      debug("should start the mq");
-
-      var subscriber = new Subscriber(app, mqOptions);
-      subscriber.getEventEmitter().once('message', onIncomingMessage);
-
-      subscriber.start()
-      .then(function() {
-        debug("started");
-        publisher.publish('', 'Ciao');
-      })
-      .catch(done);
-
-      function onIncomingMessage(message) {
-        debug("onIncomingMessage ", message.fields);
-
-        assert(message);
-        assert(message.content);
-        assert(message.content.length > 0);
-        subscriber.ack(message);
-        done();
-      }
-    });
-    it('should send and receive 10 messages', function(done) {
-      debug("should start the mq");
-      var numMessage = 0;
-      var numMessageToSend = 10;
-
-      var subscriber = new Subscriber(app, mqOptions);
-      subscriber.getEventEmitter().on('message', onIncomingMessage);
-
-      subscriber.start()
-      .then(function() {
-        debug("started");
-        _.times(numMessageToSend, function(n){
-          publisher.publish('', 'Ciao ' + n);
-        });
-
-      })
-      .catch(done);
-
-      function onIncomingMessage(message) {
-        debug("onIncomingMessage ", message.fields);
-        subscriber.ack(message);
-
-        numMessage++;
-        debug("onIncomingMessage ", numMessage);
-        if(numMessage >= numMessageToSend){
-          subscriber.getEventEmitter().removeListener('message', onIncomingMessage);
-          done();
-        }
-      }
-    });
-  });
 });

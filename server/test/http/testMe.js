22309ab41a8fdcb395f2f3d3ba97d6023b84a6b7
Frederic Heem <frederic.heem@gmail.com>
2015-10-06 15:40:36 +0200

    testMe.js es7

--- server/test/http/testMe.js
+++ server/test/http/testMe.js
@@ -1,30 +1,25 @@
-const assert = require('assert');
+import assert from 'assert';
 
 describe('Users', function() {
   let client;
   let testMngr = require('../testManager');
   this.timeout(20e3);
-  before(done => {
-    testMngr.start().then(done, done);
+  before(async () => {
+      await testMngr.start();
   });
-  after(done => {
-    testMngr.stop().then(done, done);
+  after(async () => {
+      await testMngr.stop();
   });
 
-  describe('User Basic ', () => {
-    before(done => {
+  describe('User Basic ', async () => {
+    before(async () => {
       client = testMngr.client('alice');
       assert(client);
-      client.login().then(res => {
-        console.log(res);
-        assert(res);
-      }).then(done, done);
+      await client.login();
     });
-    it('should get me', done => {
-      return client.get('v1/me').then(me => {
-        assert(me);
-        console.log(me);
-      }).then(done, done);
+    it('should get me', async () => {
+      let me = await client.get('v1/me');
+      assert(me);
     });
   });
 });

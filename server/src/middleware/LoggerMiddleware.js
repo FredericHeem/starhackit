19b20c35c76b1ca9a175645d3adffbaf4ac40e11
Frederic Heem <frederic.heem@gmail.com>
2015-10-06 19:08:45 +0200

    log in LoggerMiddleware

--- server/src/middleware/LoggerMiddleware.js
+++ server/src/middleware/LoggerMiddleware.js
@@ -1,9 +1,10 @@
 import Log from 'logfilename';
 let log = new Log(__filename);
 
-export default function LoggerMiddleware(expressApp, config){
+export default function LoggerMiddleware(expressApp){
+  log.debug("LoggerMiddleware");
   expressApp.use(function(req, res, next) {
-    log.debug("url: " + req.url);
+    log.debug("url: ", req.url);
     next();
   });
 }

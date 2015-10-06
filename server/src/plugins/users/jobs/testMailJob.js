7d7c4f77c8575ba1f065b3bd4967533e9570d6a4
Frederic Heem <frederic.heem@gmail.com>
2015-10-06 16:23:05 +0200

    add testMailJob.js

---
+++ server/src/plugins/users/jobs/testMailJob.js
@@ -0,0 +1,26 @@
+import assert from 'assert';
+import {Publisher} from 'rabbitmq-pubsub';
+import MailJob from './MailJob';
+
+describe('MailJob', function() {
+  //let testMngr = require('../testManager');
+  let publisher;
+
+  before(async () => {
+      //await testMngr.start();
+      publisher = new Publisher({exchange:"user"});
+      await publisher.start();
+  });
+  after(async () => {
+      await publisher.stop();
+      //await testMngr.stop();
+  });
+
+  describe('StartStop', function() {
+    it('start and stop the MailJob', async () => {
+      let mailJob = new MailJob();
+      await mailJob.start();
+      await mailJob.stop();
+    });
+  });
+});

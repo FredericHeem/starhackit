c9bcc94026d776464af369b8acdeffe29c778ed5
Frederic Heem <frederic.heem@gmail.com>
2015-10-07 22:28:08 +0200

    test facebook auth

--- server/src/plugins/users/auth-strategy/FacebookStrategy.js
+++ server/src/plugins/users/auth-strategy/FacebookStrategy.js
@@ -3,52 +3,53 @@ let config = require('config');
 
 let log = require('logfilename')(__filename);
 
-export function verify(models, req, accessToken, refreshToken, profile, done){
+export async function verify(models, req, accessToken, refreshToken, profile) {
   log.debug("authentication reply from fb");
   //log.debug(accessToken);
   log.debug(JSON.stringify(profile, null, 4));
 
-  if (req.user) {
-    log.error("TODO");
-    done(null, req.user);
-  } else {
-    models.User.find({where:{facebookId: profile.id}})
-    .then(function(user) {
-      if (user) {
-        log.debug("user already exist");
-        return done(null, user.get());
-      } else {
-        log.debug("no fb profile registered");
-        models.User.find({where:{email: profile._json.email}})
-        .then(function(userByEmail) {
-          if (userByEmail) {
-            log.debug("email already registered");
-            //should update fb profile id
-            done(null, userByEmail.get());
-          } else {
-            //Create user
-            let userConfig = {
-              username: `${profile.name.givenName} ${profile.name.middleName} ${profile.name.familyName}`,
-              email: profile._json.email,
-              firstName: profile.name.givenName,
-              lastName: profile.name.familyName,
-              facebookId: profile.id
-            };
-            log.debug("creating user: ", userConfig);
-            models.User.createUserInGroups(userConfig, ["User"])
-            .then(function(res) {
-              let userCreated = res.toJSON();
-              log.info("register created new user ", userCreated);
-              done(null, userCreated);
-            })
-            .catch(done);
-          }
-        })
-        .catch(done);
-      }
-    })
-    .catch(done);
+  let user = await models.User.find({
+    where: {
+      facebookId: profile.id
+    }
+  });
+
+  if (user) {
+    log.debug("user already exist");
+    return {
+      user: user.toJSON()
+    };
   }
+  log.debug("no fb profile registered");
+  let userByEmail = await models.User.find({
+    where: {
+      email: profile._json.email
+    }
+  });
+
+  if (userByEmail) {
+    log.debug("email already registered");
+    //should update fb profile id
+    return {
+      user: userByEmail.toJSON()
+    };
+  }
+
+  //Create user
+  let userConfig = {
+    username: `${profile.name.givenName} ${profile.name.middleName} ${profile.name.familyName}`,
+    email: profile._json.email,
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+    facebookId: profile.id
+  };
+  log.debug("creating user: ", userConfig);
+  user = await models.User.createUserInGroups(userConfig, ["User"]);
+  let userCreated = user.toJSON();
+  log.info("register created new user ", userCreated);
+  return {
+    user: userCreated
+  };
 }
 
 export function register(passport, models) {
@@ -56,17 +57,21 @@ export function register(passport, models) {
   let authenticationFbConfig = config.authentication.facebook;
   if (authenticationFbConfig && authenticationFbConfig.clientID) {
     log.info("configuring facebook authentication strategy");
-    let facebookStrategy = new FacebookStrategy(
-    {
-      clientID: authenticationFbConfig.clientID,
-      clientSecret: authenticationFbConfig.clientSecret,
-      callbackURL: authenticationFbConfig.callbackURL,
-      profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
-      enableProof: false
-    },
-    function (req, accessToken, refreshToken, profile, done){
-      verify(models, req, accessToken, refreshToken, profile, done);
-    });
+    let facebookStrategy = new FacebookStrategy({
+        clientID: authenticationFbConfig.clientID,
+        clientSecret: authenticationFbConfig.clientSecret,
+        callbackURL: authenticationFbConfig.callbackURL,
+        profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified'],
+        enableProof: false
+      },
+      function (req, accessToken, refreshToken, profile, done) {
+        try {
+          let res = verify(models, req, accessToken, refreshToken, profile);
+          done(res.err, res.user);
+        } catch(err){
+          done(err);
+        }
+      });
     passport.use('facebook', facebookStrategy);
   }
 };

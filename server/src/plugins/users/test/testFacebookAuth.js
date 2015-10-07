bcff001b6ea6531f61eead6a6116ca55c51e4052
Frederic Heem <frederic.heem@gmail.com>
2015-10-07 23:21:42 +0200

    lint free

--- server/src/plugins/users/test/testFacebookAuth.js
+++ server/src/plugins/users/test/testFacebookAuth.js
@@ -20,7 +20,7 @@ describe('Facebook', function(){
     let res = await client.get("v1/auth/facebook");
     assert(res);
   });
-  
+
   it('should invoke callback', async () => {
     let res =  await client.get("v1/auth/facebook/callback");
     assert(res);

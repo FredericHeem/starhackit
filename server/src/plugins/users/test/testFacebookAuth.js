f7334039144f5fc9f0917ddefa61bfda7cca71ce
Frederic Heem <frederic.heem@gmail.com>
2015-10-07 22:57:28 +0200

    remove console.log from testFacebookAuth.js

--- server/src/plugins/users/test/testFacebookAuth.js
+++ server/src/plugins/users/test/testFacebookAuth.js
@@ -17,13 +17,12 @@ describe('Facebook', function(){
   });
 
   it('should login', async () => {
-    let res =  await client.get("v1/auth/facebook");
+    let res = await client.get("v1/auth/facebook");
     assert(res);
-    console.log(res);
   });
+  
   it('should invoke callback', async () => {
     let res =  await client.get("v1/auth/facebook/callback");
     assert(res);
-    console.log(res);
   });
 });

559b597d31f9d1fb5737f8e543d1a3de3537a8c4
Frederic Heem <frederic.heem@gmail.com>
2015-10-08 22:08:50 +0200

    testMailJob

--- server/src/plugins/users/jobs/MailJob.js
+++ server/src/plugins/users/jobs/MailJob.js
@@ -1,32 +1,40 @@
-import util from 'util';
+//import util from 'util';
 import {Subscriber} from 'rabbitmq-pubsub';
-
+import nodemailer from 'nodemailer';
+import config from 'config';
 let log = require('logfilename')(__filename);
 
 const subscriberOptions = {
-  exchange:'user',
-  queueName: 'new'
+  exchange: 'user',
+  queueName: 'user.new'
 };
 
 export default class MailJob {
   constructor(){
     log.info("MailJob subscriberOptions: ", subscriberOptions);
     this.subscriber = new Subscriber(subscriberOptions);
+    if(config.mail.options){
+      this.transporter = nodemailer.createTransport(config.mail.options);
+    } else {
+      log.warn("no mail configuration");
+    }
   }
 
   async start(){
     await this.subscriber.start(this._onIncomingMessage.bind(this));
   }
+
   async stop(){
     await this.subscriber.stop();
   }
-  _sendEmail(user){
-    log.debug("sendEmail to user ", user);
-    return Promise.resolve();
+
+  async _sendEmail(type, user){
+    log.debug("sendEmail %s to user ", type, user);
   }
 
-  _onIncomingMessage(message) {
-    log.error("onIncomingMessage user: ", message.content.toString());
+  async _onIncomingMessage(message) {
+    log.info("onIncomingMessage content: ", message.content.toString());
+    log.info("onIncomingMessage : ", JSON.stringify(message.fields));
     let user;
 
     try {
@@ -35,20 +43,19 @@ export default class MailJob {
     catch(error){
       log.error("cannot convert message");
       this.subscriber.ack(message);
-      //super.getEventEmitter().emit('error', error);
       return;
     }
 
-    return this._sendEmail(user)
-    .then(function(res) {
-      log.info("mail sent: ", res);
+    try {
+      await this._sendEmail('register', user);
+      log.info("email mail sent: ");
       this.subscriber.ack(message);
-      //super.getEventEmitter().emit("success", res);
-    })
-    .catch(function(error) {
-      log.error(util.inspect(error));
-      this.subscriber.nack(message);
-      //super.getEventEmitter().emit('error', error);
-    });
+    }
+    catch(error){
+      log.error("error sending mail: ", error);
+      // TODO nack or ack ?
+      this.subscriber.ack(message);
+      return;
+    }
   }
 }
--- server/src/plugins/users/jobs/testMailJob.js
+++ server/src/plugins/users/jobs/testMailJob.js
@@ -1,25 +1,51 @@
 //import assert from 'assert';
+import sinon from 'sinon';
 import {Publisher} from 'rabbitmq-pubsub';
 import MailJob from './MailJob';
+import config from 'config';
 
 describe('MailJob', function() {
   let publisher;
+  let sandbox;
 
   before(async () => {
-      //await testMngr.start();
-      publisher = new Publisher({exchange:"user"});
+      publisher = new Publisher({exchange:"user"}, config.log);
       await publisher.start();
   });
+
   after(async () => {
       await publisher.stop();
-      //await testMngr.stop();
   });
 
-  describe('StartStop', function() {
-    it('start and stop the MailJob', async () => {
+  beforeEach(function(done) {
+    sandbox = sinon.sandbox.create();
+    done();
+  });
+
+  afterEach(function(done) {
+    sandbox.restore();
+    done();
+  });
+
+  let user = {
+    email:'joe@mymail.com'
+  };
+
+  describe('Basic', function() {
+    it('send email directly', async () => {
       let mailJob = new MailJob();
+
+      let emailType = 'register';
+      mailJob._sendEmail(emailType, user);
+    });
+    it('start, publish and stop the MailJob', async (done) => {
+      let mailJob = new MailJob();
+      sinon.stub(mailJob, "_sendEmail", () => {
+        //console.log("_sendEmail has been called");
+        done();
+       });
       await mailJob.start();
-      await mailJob.stop();
+      await publisher.publish("user.new", JSON.stringify(user));
     });
   });
 });

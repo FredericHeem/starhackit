a9f84b8145bd7b12a5673a2563b534107ce099dc
Frederic Heem <frederic.heem@gmail.com>
2015-10-09 17:13:07 +0200

    no longer skip fb test

--- server/src/plugins/users/test/testFacebookAuth.js
+++ server/src/plugins/users/test/testFacebookAuth.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import {Client} from 'restauth';
 import testMngr from '~/test/testManager';
 
-describe.skip('Facebook', function(){
+describe('Facebook', function(){
   let client;
 
   before(async () => {

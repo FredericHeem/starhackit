b502c1b42f8fc54df043c372dcee214cc1fc7abb
Frederic Heem <frederic.heem@gmail.com>
2015-11-03 18:06:19 +0000

    add missing JSON.stringify in webpack when setting NODE_ENV

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -14,7 +14,7 @@ var webpackProdConfig = {
     plugins: [
         new webpack.DefinePlugin( {
             'process.env': {
-                NODE_ENV: process.env.NODE_ENV || 'production'
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
             }
         } ),
         new webpack.optimize.DedupePlugin(),

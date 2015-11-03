3235aa108dbc83f78bf514a332a85609f1bc2146
Frederic Heem <frederic.heem@gmail.com>
2015-11-03 18:05:24 +0000

    add fallback for window.location.origin not implemente on IE11

--- client/src/app/utils/baseUrl.js
+++ client/src/app/utils/baseUrl.js
@@ -2,6 +2,10 @@
 
 import config from 'config';
 
+if (!window.location.origin) {
+    window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+}
+
 export default function( url ) {
     var fullUrl = location.origin + config.apiUrl +  url;
 

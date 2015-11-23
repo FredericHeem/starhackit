a9daee2779f9f91560144f464a79ac55b933b5ac
Frederic Heem <frederic.heem@gmail.com>
2015-11-23 19:35:02 +0000

    forgot to use route.use instead of route.all for the user router

--- server/src/plugins/users/user/UserRouter.js
+++ server/src/plugins/users/user/UserRouter.js
@@ -21,8 +21,8 @@ export function UserHttpController(app){
 export default function UserRouter(app, /*auth*/){
   let router = new Router();
   let userHttpController = UserHttpController(app);
-  router.all(app.server.auth.isAuthenticated);
-  router.all(app.server.auth.isAuthorized);
+  router.use(app.server.auth.isAuthenticated);
+  router.use(app.server.auth.isAuthorized);
   router.get('/', userHttpController.getAll);
   router.get('/:id', userHttpController.getOne);
 

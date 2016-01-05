552bde384da8c7f6b95727bd63c6578b0b16ec38
Frederic Heem <frederic.heem@gmail.com>
2016-01-05 21:58:00 +0000

    paginate users WIP

--- client/src/app/parts/admin/usersComponent.js
+++ client/src/app/parts/admin/usersComponent.js
@@ -3,43 +3,67 @@ import React from 'react';
 import Reflux from 'reflux';
 import usersStore from './usersStore';
 import usersActions from './usersActions';
-import {Table} from 'reactabular';
+import {
+    Table
+} from 'reactabular';
+import Paginator from 'react-pagify';
 
 import Debug from 'debug';
 let debug = new Debug("components:users");
 
-export default React.createClass( {
-    mixins: [
-        Reflux.connect(usersStore, 'users')
-    ],
-    getInitialState() {
+export default React.createClass({
+    mixins: [Reflux.connect(usersStore, 'users')],
+    getInitialState () {
         return {
             columns: [
                 {
                     property: 'username',
-                    header: 'Username',
-                },
-                {
+                    header: 'Username'
+                }, {
                     property: 'id',
                     header: 'Id'
-                },
-                {
+                }, {
                     property: 'fistName',
-                    header: 'First Name',
+                    header: 'First Name'
                 }
             ],
+            pagination: {
+                page: 0,
+                perPage: 4
+            }
         };
     },
-    componentDidMount(){
+    componentDidMount () {
         usersActions.getUsers();
     },
-    render() {
+    render () {
         debug('render');
         let columns = this.state.columns || [];
         let data = this.state.users || [];
 
+        let pagination = this.state.pagination;
+        let paginated = Paginator.paginate(data, pagination);
         return (
-            <Table className='pure-table pure-table-striped' columns={columns} data={data}/>
+            <div>
+                <Table className='pure-table pure-table-striped'
+                    columns={columns}
+                    data={paginated.data}/>
+                <div className='controls'>
+                    <div className='pagination'>
+                        <Paginator
+                            page={paginated.page}
+                            pages={paginated.amount}
+                            beginPages={3}
+                            endPages={3}
+                            onSelect={this.onSelectPaginator}/>
+                    </div>
+                </div>
+            </div>
         );
+    },
+    onSelectPaginator (page) {
+        let pagination = this.state.pagination || {};
+        pagination.page = page;
+        this.setState({pagination: pagination});
     }
-} );
+});

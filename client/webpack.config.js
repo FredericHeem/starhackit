6b56feb3209bf22bb263089df99ff884c7dd7ee5
Frederic Heem <frederic.heem@gmail.com>
2016-02-07 21:56:00 +0000

    also extract css

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -99,7 +99,8 @@ module.exports = function ( options ) {
         },
         module: {
             loaders: [
-                { test: /\.css$/, loader: 'style-loader!css-loader' },
+                { test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
                 { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
                 { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
                 {

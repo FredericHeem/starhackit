18d1ab5a54d53116929ff42ff0b8aa709a799d82
Frederic Heem <frederic.heem@gmail.com>
2016-02-12 22:43:29 +0000

    debug i18n, add fr

--- client/src/app/utils/i18n.js
+++ client/src/app/utils/i18n.js
@@ -6,12 +6,40 @@ import Debug from 'debug';
 
 let debug = new Debug("i18n");
 
+debug('navigator.languages: ', navigator.languages);
+
+const languagesDetectorOption = {
+  // order and from where user language should be detected
+    order: ['localStorage', 'navigator', 'querystring', 'cookie'],
+
+    // keys or params to lookup language from
+    lookupQuerystring: 'lng',
+    lookupCookie: 'i18next',
+    lookupLocalStorage: 'i18nextLng',
+
+    // cache user language on
+    caches: ['localStorage', 'cookie'],
+
+    // optional expire and domain for set cookie
+    //cookieMinutes: 10,
+    //cookieDomain: 'myDomain'
+};
+
 export default {
     load() {
         return new Promise((resolve/*, reject*/) => {
+
+            i18next.on('languageChanged', function(lng) {
+                debug('languageChanged to ', lng);
+            });
+
+            i18next.on('initialized', function(option) {
+                debug('initialized:  ', option);
+            });
+
             i18next
-                .use(XHR)
                 .use(LanguageDetector)
+                .use(XHR)
                 .init({
                     fallbackLng: 'en',
                     // have a common namespace used around the full app
@@ -22,12 +50,13 @@ export default {
 
                     interpolation: {
                         escapeValue: false // not needed for react!!
-                    }
+                    },
+                    detection: languagesDetectorOption
                 }, (err, t) => {
                     if (err) {
                         debug('error loading i18n: ', err);
                     }
-                    debug('i18n done');
+                    debug('i18n done: ');
                     resolve(t);
                 });
         });

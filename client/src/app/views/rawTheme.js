ba45b40d0261d6c2babbcbb62450ff9bd63a0895
Frederic Heem <frederic.heem@gmail.com>
2016-02-13 18:46:56 +0000

    forgot to add rawTheme.js

---
+++ client/src/app/views/rawTheme.js
@@ -0,0 +1,24 @@
+import Colors from 'material-ui/lib/styles/colors';
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
+import Spacing from 'material-ui/lib/styles/spacing';
+import zIndex from 'material-ui/lib/styles/zIndex';
+
+export default {
+    spacing: Spacing,
+    zIndex: zIndex,
+    fontFamily: 'Roboto, sans-serif',
+    palette: {
+        primary1Color: Colors.cyan500,
+        primary2Color: Colors.cyan700,
+        primary3Color: Colors.lightBlack,
+        accent1Color: Colors.pinkA200,
+        accent2Color: Colors.grey100,
+        accent3Color: Colors.grey500,
+        textColor: Colors.darkBlack,
+        alternateTextColor: Colors.white,
+        canvasColor: Colors.white,
+        borderColor: Colors.grey300,
+        disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
+        pickerHeaderColor: Colors.cyan500,
+    }
+};

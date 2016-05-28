5e2ba96c93bd5f2d21839c0c8cfcd75563871d55
Frederic Heem <frederic.heem@gmail.com>
2016-05-28 22:36:29 +0100

    auth unit test

--- client/src/app/parts/auth/authModule.spec.js
+++ client/src/app/parts/auth/authModule.spec.js
@@ -2,12 +2,22 @@ import {assert} from 'chai';
 import App from '../../app';
 
 describe('Auth', function() {
+  let app = App();
+  let {parts, store} = app;
+  const token = 'ASDFGHJKLL'
   it('setToken', () => {
-    let app = App();
-    let {parts, store} = app;
     assert.isUndefined(store.getState().auth.token);
-    let token = 'ASDFGHJKLL'
     store.dispatch(parts.auth.actions.setToken(token));
     assert.equal(store.getState().auth.token, token);
+  });
+  it('login', () => {
+    store.dispatch(parts.auth.actions.login.ok({token: token}));
+    assert.equal(store.getState().auth.authenticated, true);
+    assert.equal(store.getState().auth.token, token);
+  });
+  it('logout', () => {
+    store.dispatch(parts.auth.actions.logout.ok());
+    assert.equal(store.getState().auth.authenticated, false);
+
   });
 });

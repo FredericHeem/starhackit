16a3a54e8482c87b4462bf9173b77d94ff3ce40e
Frederic Heem <frederic.heem@gmail.com>
2016-05-30 16:56:23 +0100

    refactor configureStore

--- client/src/app/configureStore.js
+++ client/src/app/configureStore.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
-import { applyMiddleware, compose, createStore } from 'redux'
+import { applyMiddleware, compose, createStore, combineReducers} from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger';
-import RootReducer from './rootReducer';
+import { routerReducer} from 'react-router-redux';
 
 function devTools(){
     return  window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -12,15 +12,31 @@ function logger(){
   return createLogger({});
 }
 
-export default function configureStore(modules, initialState = {}) {
-  const reducers = RootReducer(modules);
+function createReducers(modules) {
+    let reducers = _.reduce(modules, (acc, module, key) => {
+        if (module.reducers) {
+            acc[key] = combineReducers(module.reducers)
+        }
+        return acc;
+    }, {});
 
-  const middlewares = _.reduce(modules, (acc, module) => {
+    reducers.routing = routerReducer;
+
+    return combineReducers(reducers)
+}
+
+function createMiddlewares(modules){
+  return _.reduce(modules, (acc, module) => {
     if(module.middleware){
       acc.push(module.middleware)
     }
     return acc
   }, []);
+}
+
+export default function configureStore(modules, initialState = {}) {
+  const reducers = createReducers(modules);
+  const middlewares = createMiddlewares(modules)
 
   const store = createStore(
     reducers,

053609adceb9e1e5de3ac6c8f7ab52a5680adcce
Frederic Heem <frederic.heem@gmail.com>
2016-06-26 22:40:22 +0100

    more e2e for register

--- client/test/nightwatch/test/testRegister.js
+++ client/test/nightwatch/test/testRegister.js
@@ -18,4 +18,31 @@ describe('Register', function () {
             .click('@submit')
             .waitForElementVisible('.registration-request-complete', 5000)
     });
+    it('missing username', function (client) {
+        client.page.register().navigate()
+            .waitForElementVisible('.register-form', 5000)
+            .setValue('@emailInput', 'alice@mail.com')
+            .setValue('@passwordInput', 'password')
+            .click('@submit');
+
+        client.assert.containsText('.register-form', 'The username is required')
+    });
+    it('missing email', function (client) {
+        client.page.register().navigate()
+            .waitForElementVisible('.register-form', 5000)
+            .setValue('@usernameInput', 'alice')
+            .setValue('@passwordInput', 'password')
+            .click('@submit');
+
+        client.assert.containsText('.register-form', 'The email is required')
+    });
+    it('missing password', function (client) {
+        client.page.register().navigate()
+            .waitForElementVisible('.register-form', 5000)
+            .setValue('@usernameInput', 'alice')
+            .setValue('@emailInput', 'alice@mail.com')
+            .click('@submit');
+
+        client.assert.containsText('.register-form', 'The password is required')
+    });
 });

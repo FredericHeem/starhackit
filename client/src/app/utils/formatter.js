d51a1fa42e5f9be3dbc7aaa031b86eba6b00a9c6
Frederic Heem <frederic.heem@gmail.com>
2016-08-02 22:44:08 +0100

    add date formatter based on Intl

--- client/src/app/utils/formatter.js
+++ client/src/app/utils/formatter.js
@@ -1,5 +1,10 @@
 export default (locale = 'en') => {
+  const dateTimeOptions = {year: 'numeric', month: 'short', day: 'numeric'};
+
   return {
+    dateTime: (date) => {
+      return new Intl.DateTimeFormat(locale, dateTimeOptions).format(new Date(date));
+    },
     money: (amount, currency) => {
       return new Intl.NumberFormat(locale, {
         style: 'currency',
--- client/src/app/utils/test/formatter.spec.js
+++ client/src/app/utils/test/formatter.spec.js
@@ -4,6 +4,7 @@ import {
   assert
 } from 'chai';
 const moneyFixtures = require('./money.fixture');
+const dateFixtures = require('./date.fixture');
 
 describe('Formatter', function () {
   describe('money', function () {
@@ -15,6 +16,19 @@ describe('Formatter', function () {
         _.each(fixtures, fixture => {
           const [amount, currency, result] = fixture;
           assert.equal(formatter(locale).money(amount, currency), result);
+        })
+      })
+    });
+  })
+  describe('date', function () {
+    it('2016-08-02', () => {
+      assert.equal(formatter('en').dateTime('2016-08-02'), "Aug 2, 2016")
+    });
+    it('all locales', () => {
+      _.each(dateFixtures.default, (fixtures, locale) => {
+        _.each(fixtures, fixture => {
+          const [dateISO, result] = fixture;
+          assert.equal(formatter(locale).dateTime(dateISO), result);
         })
       })
     });

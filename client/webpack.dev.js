66ffd8f0753c4200f83993bbc8c5bb1b343ecf8a
Frederic Heem <frederic.heem@gmail.com>
2016-11-23 20:32:16 +0000

    webpack-hud is back

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -12,6 +12,7 @@ var webpackDevConfig = {
             app: [
                 'webpack-dev-server/client?http://localhost:8080',
                 'webpack/hot/only-dev-server',
+                'webpack-hud',
                 './src/app/index.js'
             ]
         }

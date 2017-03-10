5222f84c59503c360289a65acc707a93c04cc45a
Frederic Heem <frederic.heem@gmail.com>
2017-03-10 19:54:05 +0000

    use async await for profile

--- client/src/app/services/rules.js
+++ client/src/app/services/rules.js
@@ -2,6 +2,6 @@
 export default {
     username: [ 'required', 'alphaDash', 'minLength:3', 'maxLength:64'],
     password: [ 'required', 'alphaDash', 'minLength:6', 'maxLength:64' ],
-    biography: [ 'minLength:0', 'maxLength:2000' ],
+    biography: [ 'exists', 'minLength:0', 'maxLength:2000' ],
     email: [ 'email', 'required', 'minLength:3', 'maxLength:64' ]
 }

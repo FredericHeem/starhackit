e885c007ed74f8d97919f1c9c566bf8770ab355c
Frederic Heem <frederic.heem@gmail.com>
2017-05-13 20:06:08 +0100

    glamorous footer

--- client/src/app/parts/core/components/footer.js
+++ client/src/app/parts/core/components/footer.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import './footer.styl';
+import glamorous from 'glamorous';
 
-export default({tr}) => {
-  function Footer({version}) {
+export default ({ tr }) => {
+  const FooterView = glamorous('footer')({
+    position: 'absolute',
+    bottom: 0,
+    paddingTop: 5,
+    width: '100%',
+    boxShadow: '0px -1px 1px rgba(0, 0, 0, 0.3)'
+  });
+  function Footer({ version }) {
     return (
-      <footer className="footer hidden-print">
+      <FooterView className="hidden-print">
         <div className="container">
           <div className="row">
             <div className="content text-center">
-              <div>{tr.t('StarHackIt is the starting point to build a full stack web application')}</div>
+              <div>
+                {tr.t('StarHackIt is the starting point to build a full stack web application')}
+              </div>
               <div>
                 {tr.t('Get the source code at ')}
-                <a href="https://github.com/FredericHeem/starhackit" rel="noopener noreferrer" target="_blank">{tr.t('GitHub')}</a>
+                <a
+                  href="https://github.com/FredericHeem/starhackit"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {tr.t('GitHub')}
+                </a>
               </div>
               <div>
                 {version}
@@ -19,8 +34,8 @@ export default({tr}) => {
             </div>
           </div>
         </div>
-      </footer>
-    )
+      </FooterView>
+    );
   }
   return Footer;
-}
+};

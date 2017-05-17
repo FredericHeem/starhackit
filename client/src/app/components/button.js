61759fca689a7fb1b1999f9f693daa5f492d4aed
Frederic Heem <frederic.heem@gmail.com>
2017-05-17 21:28:50 +0100

    update button and input

--- client/src/app/components/button.js
+++ client/src/app/components/button.js
@@ -1,61 +1,89 @@
 import React from 'react';
 import glamorous from 'glamorous';
 
+const ripple = {
+  position: 'relative',
+  overflow: 'hidden',
+  transform: 'translate3d(0, 0, 0)',
+  ':after': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    pointerEvents: 'none',
+    backgroundImage: 'radial-gradient(circle, #000 10%, transparent 10%)',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: '50%',
+    transform: 'scale(10,10)',
+    opacity: 0,
+    transition: 'transform .5s, opacity 1s',
+  },
+  ':active:after': {
+    transform: 'scale(0,0)',
+    opacity: 0.2,
+    transition: '0s',
+    backgroundColor: 'red',
+  },
+};
+
 export default ({ theme }) => {
   const { palette } = theme;
-  const ButtonView = glamorous('button')({
-    padding: 8,
-    backgroundColor: palette.canvasColor,
-
-    cursor: 'pointer',
-    ':hover': {
-      backgroundColor: palette.accent2Color,
+  const styles = {
+    root: {
+      padding: '0.5rem',
+      backgroundColor: palette.canvasColor,
+      display: 'inline-flex',
+      alignItems: 'center',
+      textTransform: 'uppercase',
+      textAlign: 'center',
+      transition: 'background-color 0.3s',
+      outline: 'none',
+      ':hover': {
+        backgroundColor: palette.accent2Color,
+      },
+      color: palette.textColor,
     },
-    display: 'inline-flex',
-    alignItems: 'center',
-    fontSize: 14,
-    textTransform: 'uppercase'
-  });
-  const FlatButton = glamorous(ButtonView)({
-    border: `0px solid ${palette.borderColor}`,
-  });
-  const RaisedButton = glamorous(ButtonView)({
-    border: `1px solid ${palette.borderColor}`,
-    boxShadow: 'rgba(0, 0, 0, 0.1) 0px 1px 6px, rgba(0, 0, 0, 0.1) 0px 1px 4px',
-  });
-  const A = glamorous('a')({
-    textDecoration: 'none',
-    color: palette.textColor,
-    display: 'inline-flex',
-    alignItems: 'center',
-    textAlign: 'center',
-    width: '100%'
-  });
+    button: {
+      cursor: 'pointer',
+    },
+    a: {
+      textDecoration: 'none',
+      boxSizing: 'border-box',
+    },
+    flat: {
+      borderWidth: 0,
+    },
+    raised: {
+      border: `1px solid ${palette.borderColor}`,
+      boxShadow: 'rgba(0, 0, 0, 0.1) 0px 1px 6px, rgba(0, 0, 0, 0.1) 0px 1px 4px',
+    },
+    label: {
+      width: '100%',
+    },
+    icon: {
+      padding: '0.4rem',
+    },
+  };
+  const ButtonView = glamorous('button')(styles.root, styles.button);
+  const AnchorView = glamorous('a')(styles.root, styles.a);
+  const LabelView = glamorous('span')(styles.label);
+  const IconView = glamorous('span')(styles.icon);
 
-  const LabelView = glamorous('span')({
-    width: '100%'
-  });
-  const IconView = glamorous('span')({
-    padding: 0,
-  });
-
-  function Button(props) {
+  return function Button(props) {
     const { flat, label, href, icon, ...otherProps } = props;
-    const TheButton = flat ? FlatButton : RaisedButton
-    if (href) {
-      return (
-        <TheButton {...otherProps}>
-          <A href={href}>
-            {icon ? <IconView>{icon}</IconView> : null}<LabelView>{label}</LabelView>
-          </A>
-        </TheButton>
-      );
-    }
+    const TheButton = glamorous(href ? AnchorView : ButtonView)(
+      flat ? styles.flat : styles.raised,
+      ripple,
+    );
+
     return (
-      <TheButton {...otherProps}>
-        {icon ? <IconView>{icon}</IconView> : null} <LabelView>{label}</LabelView>
+      <TheButton href={href} {...otherProps}>
+        {icon ? <IconView>{icon}</IconView> : null}
+        <LabelView>{label}</LabelView>
       </TheButton>
     );
-  }
-  return Button;
+  };
 };
--- client/src/app/components/input.js
+++ client/src/app/components/input.js
@@ -1,91 +1,102 @@
-import React from "react";
-import { observable } from "mobx";
-import { observer } from "mobx-react";
-import glamorous from "glamorous";
+import React from 'react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import glamorous from 'glamorous';
+import { keyframes } from 'glamor';
 
 export default ({ theme }) => {
   const { palette } = theme;
   const store = observable({
-    focus: false
+    focus: false,
+  });
+
+  const animation = keyframes({
+    '0%': { transform: 'scale(0)', opacity: 0 },
+    '100%': { transform: 'scale(1)', opacity: 1 },
   });
 
   const styles = {
     root: {
-      margin: "auto",
+      margin: 'auto',
+      width: '256',
       padding: 0,
-      width: 200,
-      position: "relative"
+      position: 'relative',
     },
     label: {
       root: {
-        fontSize: 14,
-        position: "relative",
-        color: palette.secondaryTextColor
+        fontSize: '0.9rem',
+        position: 'relative',
+        color: palette.secondaryTextColor,
+        transition: 'color 1s',
       },
       focus: {
-        color: palette.primary1Color
-      }
+        color: palette.primary1Color,
+      },
     },
     input: {
-      position: "relative",
+      position: 'relative',
       margin: 0,
       paddingBottom: 4,
       paddingTop: 4,
-      outline: "none",
-      width: "100%",
-      border: "none",
-      fontSize: 16
+      outline: 'none',
+      width: '100%',
+      border: 'none',
+      fontSize: '1.2rem',
       //color: palette.textColor,
     },
     underline: {
       root: {
-        position: "absolute",
-        width: "100%",
+        position: 'absolute',
+        width: '100%',
         margin: 0,
-        transition: "all 0.5s ease-in-out",
-        border: `1px solid ${palette.borderColor}`
+        transition: 'all 0.5s ease-in-out',
+        border: `1px solid`,
+        transform: 'scaleX(0)',
+      },
+      static: {
+        borderColor: palette.borderColor,
+        animation: `${animation} 1s`,
+        transform: 'scaleX(1)',
       },
       focusOff: {
-        backgroundColor: palette.primary1Color,
-        transform: "scaleX(0)",
-        padding: 2
+        borderColor: palette.primary1Color,
       },
-      focusOn: {
-        transform: "scaleX(1)"
+      errorOff: {
+        borderColor: 'red',
+      },
+      show: {
+        transform: 'scaleX(1)',
       },
       disabled: {
-        border: `1px dotted ${palette.borderColor}`
-      }
-    }
+        border: `1px dotted ${palette.borderColor}`,
+      },
+    },
+    error: {
+      paddingTop: 6,
+      color: 'red',
+    },
   };
-  const InputContainerView = glamorous("div")(styles.root);
-  const InputView = glamorous("input")(styles.input);
-  const UnderlineView = glamorous("hr")(styles.underline.root);
+  const InputContainerView = glamorous('div')(styles.root);
+  const LabelView = glamorous('label')(styles.label.root);
+  const InputView = glamorous('input')(styles.input);
+  const UnderlineView = glamorous('hr')(styles.underline.root);
+  const UnderlineStaticView = glamorous(UnderlineView)(styles.underline.static);
   const UnderlineFocusView = glamorous(UnderlineView)(
-    styles.underline.focusOff
+    styles.underline.focusOff,
   );
+  const UnderlineErrorView = glamorous(UnderlineView)(
+    styles.underline.errorOff,
+  );
+  const ErrorView = glamorous('div')(styles.error);
 
-  function Underline({ disabled }) {
-    return <UnderlineView css={disabled && styles.underline.disabled} />;
-  }
-  function UnderlineFocus({ focus }) {
-    return <UnderlineFocusView css={focus && styles.underline.focusOn} />;
-  }
-
-  const LabelView = glamorous("label")(styles.label.root);
-
-  function Label({ label, id, focus }) {
-    return (
-      <LabelView htmlFor={id} css={focus && styles.label.focus}>
-        {label}
-      </LabelView>
-    );
-  }
   return observer(function Input(props) {
-    const { id, label, onChange, ...otherProps } = props;
+    const { id, label, onChange, error, ...otherProps } = props;
     return (
       <InputContainerView>
-        {label && <Label focus={store.focus} id={id} label={label} />}
+        {label &&
+          <LabelView htmlFor={id} css={store.focus && styles.label.focus}>
+            {label}
+          </LabelView>}
         <InputView
           id={id}
           onChange={onChange}
@@ -97,8 +108,15 @@ export default ({ theme }) => {
           }}
           {...otherProps}
         />
-        <Underline disabled={otherProps.disabled} />
-        <UnderlineFocus focus={store.focus} />
+        <UnderlineStaticView
+          css={otherProps.disabled && styles.underline.disabled}
+        />
+        <UnderlineFocusView css={store.focus && styles.underline.show} />
+        <UnderlineErrorView css={error && styles.underline.show} />
+        {error &&
+          <ErrorView>
+            {error}
+          </ErrorView>}
       </InputContainerView>
     );
   });

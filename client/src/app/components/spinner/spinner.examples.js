3c2a85a0a8c11696d84a1ddc44bd24dc2b424afe
Frederic Heem <frederic.heem@gmail.com>
2017-05-25 21:59:39 +0100

    add spinner animated by css

--- client/src/app/components/spinner/spinner.examples.js
+++ client/src/app/components/spinner/spinner.examples.js
@@ -2,14 +2,18 @@ import React from "react";
 import spinner from "./spinner";
 
 export default context => {
-  const { tr } = context;
+  const { tr, theme } = context;
+  const { palette } = theme;
   const Spinner = spinner(context);
   return function SpinnerExamples() {
     return (
       <div>
         <h3>{tr.t("Spinner")}</h3>
+        
         <Spinner />
-        <Spinner size={20} />
+        <Spinner size={30} />
+        <Spinner size={40} color={palette.primary} />
+        <Spinner size={50} color={palette.secondary} />
       </div>
     );
   };
--- client/src/app/components/spinner/spinner.js
+++ client/src/app/components/spinner/spinner.js
@@ -1,36 +1,35 @@
 import React from "react";
+import glamorous from "glamorous";
+import { keyframes } from "glamor";
 
-export default () => function Spinner() {
+const animation = keyframes({
+  "0%": { transform: "rotate(0deg)" },
+  "100%": { transform: "rotate(360deg)" }
+});
+
+const SvgView = glamorous("svg")({
+  animation: `${animation} 4s linear infinite`
+});
+
+export default () => function Spinner({ size = 26, color = "black" }) {
+    const radius = size / 2;
     return (
-      <svg
-        version="1.1"
+      <SvgView
         id="loader-1"
-        x="0px"
-        y="0px"
-        width="40px"
-        height="40px"
-        viewBox="0 0 40 40"
-        enableBackground="new 0 0 40 40"
+        width={size}
+        height={size}
+        viewBox={`0 0 ${size} ${size}`}
       >
-        <path
-          opacity="0.2"
-          fill="#000"
-          d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"
+        <circle
+          fill="none"
+          stroke={color}
+          strokeWidth="3"
+          strokeDasharray={`${radius / 2},${radius}`}
+          strokeDashoffset="0"
+          cx={radius}
+          cy={radius}
+          r={radius - 2}
         />
-        <path
-          fill="#000"
-          d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0C22.32,8.481,24.301,9.057,26.013,10.047z"
-        >
-          <animateTransform
-            attributeType="xml"
-            attributeName="transform"
-            type="rotate"
-            from="0 20 20"
-            to="360 20 20"
-            dur="0.5s"
-            repeatCount="indefinite"
-          />
-        </path>
-      </svg>
+      </SvgView>
     );
   };

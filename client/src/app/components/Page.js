35bf4758cc6a01bd41e2145970fbb94e0bb1df1c
Frederic Heem <frederic.heem@gmail.com>
2017-05-25 21:00:07 +0100

    use flex to display views

--- client/src/app/components/Page.js
+++ client/src/app/components/Page.js
@@ -1,8 +1,6 @@
 import glamorous from 'glamorous';
 
 export default glamorous.div({
-  maxWidth: 600,
-  width: '100%',
-  margin: 'auto',
-  padding: 20,
+  minWidth: 400,
+  maxWidth: 600
 });
--- client/src/app/components/componentGuide.js
+++ client/src/app/components/componentGuide.js
@@ -1,4 +1,5 @@
 import React from "react";
+import glamorous from "glamorous";
 import spinners from "./spinner/spinner.examples";
 import alerts from "./alert/alert.examples";
 import panels from "./panel/panel.examples";
@@ -8,23 +9,43 @@ const h = React.createElement;
 
 export default context => {
   const { tr, parts } = context;
-        
+  const ViewContainer = glamorous("div")({
+    display: "flex",
+    flexWrap: "wrap"
+  });
+
   return function ComponentGuide() {
     return (
       <div>
         <h1>{tr.t("The component guide.")}</h1>
+        <h3>
+          {tr.t(
+            "A showroom to display components and views with various properties"
+          )}
+        </h3>
         <h2>
-          {tr.t("A showroom to display components with various properties")}
+          {tr.t("Components")}
         </h2>
-        {h(alerts(context))}
-        {h(inputs(context))}
-        {h(spinners(context))}
-        {h(panels(context))}
-        {h(parts.auth.containers().login())}
-        {h(parts.auth.containers().register())}
-        {h(parts.auth.containers().logout())}
-        {h(parts.auth.containers().forgot())}
-        {h(parts.auth.containers().resetPassword())}
+        <ViewContainer>
+          {h(alerts(context))}
+          {h(inputs(context))}
+          {h(spinners(context))}
+          {h(panels(context))}
+        </ViewContainer>
+        <h2>
+          {tr.t("Authentication")}
+        </h2>
+        <ViewContainer>
+          {h(parts.auth.containers().login())}
+          {h(parts.auth.containers().register())}
+          {h(parts.auth.containers().logout())}
+          {h(parts.auth.containers().forgot())}
+          {h(parts.auth.containers().resetPassword())}
+        </ViewContainer>
+
+        <h2>
+          {tr.t("Profile")}
+        </h2>
         {h(parts.profile.containers().profile())}
       </div>
     );

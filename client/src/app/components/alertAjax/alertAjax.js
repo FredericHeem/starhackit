08ab28b2d86e6785924d3e1da36293fc6877fb53
Frederic Heem <frederic.heem@gmail.com>
2017-09-14 22:34:28 +0100

    revert AlertAjex

--- client/src/app/components/alertAjax/alertAjax.js
+++ client/src/app/components/alertAjax/alertAjax.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import Debug from 'debug';
-import alert from '../alert';
+import get from "lodash/get";
+import React from "react";
+import Debug from "debug";
+import alert from "../alert";
 
 const debug = new Debug("components:alertAjax");
 
 // A component to display Axios errors
 
-export default (context) => {
+export default context => {
   const Alert = alert(context);
 
-  function AlertAjax({error, className}) {
+  function AlertAjax({ error, className }) {
     if (!error) {
       return null;
     }
-    debug('error:', error);
-    const {status} = error.response;
-    debug('error status :', status);
-    if(![401, 422].includes(status)){
+    debug("error:", error);
+    const { status } = error.response;
+    debug("error status :", status);
+    if (![401, 422].includes(status)) {
       return null;
     }
-    
-    const {message} = error;
-    
-    return (<Alert.Danger className={className} message={message} />)
+    let message = get(error, "response.data.error.message");
+    if (!message) {
+      message = error.message;
+    }
+
+    return <Alert.Danger className={className} message={message} />;
   }
 
   return AlertAjax;
-}
+};

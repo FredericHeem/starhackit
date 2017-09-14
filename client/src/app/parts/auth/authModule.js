20545dd28b1ece0a3d761fbc7b8df46e35d07f07
Frederic Heem <frederic.heem@gmail.com>
2017-09-14 18:49:03 +0100

    add xhr-eval-chunk-webpack-plugin

--- client/src/app/parts/auth/authModule.js
+++ client/src/app/parts/auth/authModule.js
@@ -15,7 +15,7 @@ export default function(context) {
   const AsyncView = asyncView(context);
 
   function redirect() {
-    const nextPath = parse(window.location.search).nextPath || "/app/profile";
+    const nextPath = parse(window.location.search.slice(1)).nextPath || "/app/profile";
     context.history.push(nextPath);
   }
   function Stores() {

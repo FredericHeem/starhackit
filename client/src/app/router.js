84b831d8597933100482f1e710a72cd515577d9b
Frederic Heem <frederic.heem@gmail.com>
2018-03-02 18:02:13 +0000

    split router and create routes

--- client/src/app/router.js
+++ client/src/app/router.js
@@ -1,75 +1,17 @@
 import React, { createElement as h } from "react";
 import Router from "universal-router";
-import asyncView from "components/AsyncView";
 import { render } from "react-dom";
 import { parse } from "qs";
 import appView from "components/applicationView";
+import asyncView from "components/AsyncView";
+
+import createRoutes from "./routes";
 
 export default context => {
   const AlertStack = context.alertStack.View;
   const { tr, history, config } = context;
-  const AsyncView = asyncView(context);
 
-  function isAuthenticated({ pathname }) {
-    console.log("isAuthenticated ", pathname);
-    const { authenticated } = context.parts.auth.stores().auth;
-    if (!authenticated) {
-      setTimeout(() => context.history.push(`/login?nextPath=${pathname}`), 1);
-      throw new Error({ name: "redirect", status: 302 });
-    }
-  }
-
-  function createPart(partName, partCreate, routerContext) {
-    const part = partCreate.default(context);
-    context.parts[partName] = part;
-    routerContext.route.children = part.routes();
-    return routerContext.next();
-  }
-
-  const { parts } = context;
-  const routes = [
-    {
-      path: "",
-      component: () => ({
-        title: "Home",
-        component: h(AsyncView, {
-          getModule: () => import("./parts/landing/landingScreen")
-        })
-      })
-    },
-    {
-      path: "/guide",
-      component: () => ({
-        title: "Component Guide",
-        component: h(AsyncView, {
-          getModule: () => import("./components/componentGuide")
-        })
-      })
-    },
-    ...parts.auth.routes(),
-    ...parts.db.routes(),
-    ...parts.hello.routes(),
-    {
-      path: "/profile",
-      children: [],
-      load: async routerContext => {
-        isAuthenticated(routerContext);
-        const partCreate = await import("./parts/profile/profileModule");
-        return createPart("profile", partCreate, routerContext);
-      }
-    },
-    {
-      path: "/users",
-      children: [],
-      load: async routerContext => {
-        isAuthenticated(routerContext);
-        const partCreate = await import("./parts/admin/adminModule");
-        return createPart("admin", partCreate, routerContext);
-      }
-    }
-  ];
-
-  const router = new Router(routes, {
+  const router = new Router(createRoutes(context), {
     resolveRoute(routerContext, params) {
       const { route } = routerContext;
       //console.log("resolveRoute ", routerContext, params);
@@ -85,7 +27,7 @@ export default context => {
       }
       return undefined;
     }
-  });
+  })
 
   const onRenderComplete = route => {
     document.title = `${route.title} - ${config.title}`;
@@ -133,4 +75,4 @@ export default context => {
       onLocationChange(history.location);
     }
   };
-};
+}

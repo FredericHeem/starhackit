46c42a753210d13b8a0a61d209636983e6e01301
Frederic Heem <frederic.heem@gmail.com>
2018-03-04 13:45:22 +0000

    common handling for removal and substitution

--- client/src/app/components/Animate.js
+++ client/src/app/components/Animate.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 export default class DelayUnmount extends Component {
   state = {
-    previous: null,
     current: null
   };
 
@@ -18,66 +17,57 @@ export default class DelayUnmount extends Component {
     const { state } = this;
     const nextComponent = nextProps.component;
     if (nextComponent !== state.current) {
-      this.setState({ previous: state.current, current: nextComponent });
+      this.saveNode(this.nodeCurrent);
+      this.setState({ current: nextComponent });
     }
   }
 
   componentWillUnmount() {
-    const { nodeCurrent } = this;
-    if (nodeCurrent) {
-      const nodeCloned = nodeCurrent.parentNode.cloneNode(true);
-      nodeCloned.style.position = "fixed";
-      const rect = this.nodeCurrentRect;
-      nodeCloned.style.left = `${rect.left}px`;
-      nodeCloned.style.top = `${rect.top}px`;
-      document.body.appendChild(nodeCloned);
-      this.saveNode(nodeCloned);
-    }
+    this.saveNode(this.nodeCurrent, true);
   }
 
-  saveNode(node) {
+  saveNode(node, removal) {
     if (node && node.innerHTML) {
-      node.style.display = "block";
-      node.style.animation = this.props.animationHide;
+      const nodeCloned = node.cloneNode(true);
 
-      const animationEndHandler = () => {
-        node.removeEventListener("animationend", animationEndHandler);
-        node.style.display = "none";
-        while (node.firstChild) {
-          node.removeChild(node.firstChild);
-        }
-      };
+      let rect = node.firstChild.getBoundingClientRect();
+      if (rect.width === 0) {
+        rect = this.nodeCurrentRect;
+      }
+      nodeCloned.style.left = `${rect.left}px`;
+      nodeCloned.style.top = `${rect.top}px`;
+      nodeCloned.style.animation = this.props.animationHide;
+      nodeCloned.style.position = "fixed";
+      if (removal) {
+        document.body.appendChild(nodeCloned);
+      } else {
+        node.insertAdjacentElement("afterend", nodeCloned);
+      }
 
-      node.addEventListener("animationend", animationEndHandler);
+      nodeCloned.addEventListener("animationend", () =>
+        nodeCloned.parentNode.removeChild(nodeCloned)
+      );
     }
   }
   render() {
     const { state } = this;
     const me = this;
     return (
-      <div>
-        <div
-          ref={node => {
-            this.saveNode(node);
-          }}
-        >
-          {state.previous}
-        </div>
-        <div
-          ref={node => {
-            if (node && node.innerHTML) {
-              node.style.animation = this.props.animationShow;
-              const animationBeginHandler = () => {
-                node.removeEventListener("animationend", animationBeginHandler);
-              };
+      <div
+        ref={node => {
+          if (node && node.innerHTML) {
+            node.style.animation = this.props.animationShow;
+            const animationBeginHandler = () => {
+              node.removeEventListener("animationend", animationBeginHandler);
+              node.style.animation = "";
+            };
 
-              node.addEventListener("animationend", animationBeginHandler);
-              me.nodeCurrent = node;
-            }
-          }}
-        >
-          {state.current}
-        </div>
+            node.addEventListener("animationend", animationBeginHandler);
+            me.nodeCurrent = node;
+          }
+        }}
+      >
+        {state.current}
       </div>
     );
   }

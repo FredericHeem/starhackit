7d6353ae383074c4a9716efa1451d70b411e92c6
Frederic Heem <frederic.heem@gmail.com>
2018-03-10 19:40:18 +0000

    add mobile facebook authentication

---
+++ server/src/plugins/users/auth-strategy/FacebookMobileStrategy.js
@@ -0,0 +1,89 @@
+let FbMobileStrategy = require('passport-local').Strategy;
+import Axios from 'axios';
+
+let log = require('logfilename')(__filename);
+
+export async function verifyLogin(models, publisherUser, userID, token){
+  log.debug("verifyLogin ", token); 
+
+  const axios = Axios.create({
+    baseURL: "https://graph.facebook.com/",
+    timeout: 30e3
+  });
+
+  let profile;
+  try {
+    const result = await axios.get('me', {
+        params: {
+        fields: "name,email,first_name,last_name",
+        access_token: token
+      }
+    });
+    profile = result.data;
+    log.debug("profile ", profile);
+  } catch(error){
+    log.error("loginFacebook ", error);
+    return {
+      error: {
+        message: 'InvalidToken'
+      }
+    };
+  }
+
+  let authProvider = await models.AuthProvider.find({
+    where: {
+      name: 'facebook',
+      authId: profile.id
+    }
+  });
+
+  if (authProvider) {
+    log.debug("user already exist: auth ", authProvider.toJSON());
+    let user = await models.User.findByUserId(authProvider.get().user_id);
+    return {
+      user: user.toJSON()
+    };
+  }
+
+  //Create user
+  let userConfig = {
+    username: profile.name,
+    email: profile.email,
+    firstName: profile.first_name,
+    lastName: profile.last_name,
+    authProvider: {
+      name: 'facebook',
+      authId: profile.id
+    }
+  };
+  log.debug("creating user: ", userConfig);
+  let user = await models.User.createUserInGroups(userConfig, ["User"]);
+  let userCreated = user.toJSON();
+
+  log.info("register created new user ", userCreated);
+  if(publisherUser){
+    await publisherUser.publish("user.registered", JSON.stringify(userCreated));
+  }
+  return {
+    user: userCreated
+  };
+}
+
+export function register(passport, models, publisherUser) {
+  log.info("configuring facebook mobile authentication strategy");
+  let fbMobileStrategy = new FbMobileStrategy({
+      usernameField: 'userId',
+      passwordField: 'token',
+      passReqToCallback: false
+    },
+    async function (userID, token, done) {
+      try {
+        let res = await verifyLogin(models, publisherUser, userID, token);
+        done(res.error, res.user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  );
+  passport.use('facebook_mobile', fbMobileStrategy);
+};
--- server/src/plugins/users/authentication/AuthenticationRouter.js
+++ server/src/plugins/users/authentication/AuthenticationRouter.js
@@ -7,37 +7,50 @@ import config from 'config';
 
 let log = require('logfilename')(__filename);
 
+const errorMsg = (err, info) => {
+  if(info && info.message){
+    return info.message;
+  } else if(err && err.message){
+    return err.message;
+  } else {
+    return "Unknown error";
+  }
+};
+
+const localAuthCB = ctx => (err, user, info = {}) => {
+  const jwtConfig = _.defaults(config.jwt, {secret: "secret"});
+  log.debug("localAuthCB %s, %s, %s", JSON.stringify(user), JSON.stringify(info), JSON.stringify(err));
+  if (user) {
+    ctx.body = {
+        user,
+        token: jwt.sign(user, jwtConfig.secret, jwtConfig.options)
+    };
+    ctx.login(user, error => {
+      if(error){
+        log.error("login ", error);
+        throw error;
+      } else {
+        log.debug("login ok ");
+      }
+    });
+  } else {
+    ctx.status = 401;
+    ctx.body = {
+      error: {
+        message: errorMsg(err, info)
+      }
+    };
+  }
+}
+
 export function AuthenticationHttpController(app, publisherUser){
   log.debug("AuthenticationHttpController");
   let authApi = AuthenticationApi(app, publisherUser);
   let respond = app.utils.http.respond;
-  const jwtConfig = _.defaults(config.jwt, {secret: "secret"});
+
   return {
     login(ctx, next) {
-      return passport.authenticate('local', function(err, user, info/*, status*/) {
-        log.debug("login %s, %s", JSON.stringify(user), info);
-        if (user) {
-          ctx.body = {
-              user,
-              token: jwt.sign(user, jwtConfig.secret, jwtConfig.options)
-          };
-          ctx.login(user, error => {
-            if(error){
-              log.error("login ", error);
-              throw error;
-            } else {
-              log.debug("login ok ");
-            }
-          });
-        } else {
-          ctx.status = 401;
-          ctx.body = {
-            error: {
-              message: "Username and Password do not match"
-            }
-          };
-        }
-      })(ctx, next);
+      return passport.authenticate('local', localAuthCB(ctx))(ctx, next);
     },
     logout(ctx) {
       log.debug("logout");
@@ -57,7 +70,10 @@ export function AuthenticationHttpController(app, publisherUser){
     },
     async verifyResetPasswordToken(context){
       return respond(context, authApi, authApi.verifyResetPasswordToken, [context.request.body]);
-    }
+    },
+    async loginFacebook(context, next) {
+      return passport.authenticate('facebook_mobile', localAuthCB(context))(context, next);
+    },
   };
 }
 
@@ -71,9 +87,12 @@ export default function AuthenticationRouter(app, publisherUser){
   router.post('/verify_email_code', authHttpController.verifyEmailCode);
   router.post('/verify_reset_password_token', authHttpController.verifyResetPasswordToken);
 
+  // Facebook Auth from web
   router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
   router.get('/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/login', successRedirect : '/login'}));
+  passport.authenticate('facebook', { failureRedirect: '/login', successRedirect : '/login'}));
+  // Facebook Auth from mobile
+  router.post('/login_facebook', authHttpController.loginFacebook);
 
   router.get('/fidor', passport.authenticate('fidor', { scope: ['email'] }));
   router.get('/fidor/callback',

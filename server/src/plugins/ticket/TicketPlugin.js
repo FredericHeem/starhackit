13db305260f88c8be185ee9450f81687db596977
Frederic Heem <frederic.heem@gmail.com>
2018-03-17 18:37:11 +0000

    refactor Ticket plugin

--- server/src/plugins/ticket/TicketPlugin.js
+++ server/src/plugins/ticket/TicketPlugin.js
@@ -1,14 +1,62 @@
-import TicketRouter from './TicketRouter';
+import Router from "koa-66";
+import _ from "lodash";
 
-export default function ticketPlugin(app){
+const log = require("logfilename")(__filename);
 
+export default function ticketPlugin(app) {
+  const { sequelize } = app.data;
   app.data.registerModel(__dirname, `TicketModel`);
+  const { models } = sequelize;
 
-  TicketRouter(app, app.server.auth);
-  return {
-    async start(){
+  const api = {
+    getAll: {
+      pathname: "/",
+      method: "get",
+      handler: async context => {
+        console.log("getAll querystring: ", context.querystring);
+        let tickets = await models.Ticket.findAll();
+        log.debug("getAll: ", tickets);
+        context.status = 200;
+        return tickets;
+      }
     },
-    async stop(){
+    getOne: {
+      pathname: "/:id",
+      method: "get",
+      handler: async context => {
+        const { id } = context.params;
+        log.debug("getOne id:", id);
+        const ticket = await models.Ticket.findOne({ where: { id } });
+        log.debug("getOne: ", ticket);
+        if (!ticket) {
+          context.status = 404;
+          context.body = {
+            error: {
+              code: 404,
+              name: "NotFound"
+            }
+          };
+          return;
+        }
+
+        context.status = 200;
+        return ticket;
+      }
     }
+  };
+
+  let router = new Router();
+  router.use(app.server.auth.isAuthenticated);
+  //router.use(app.server.auth.isAuthorized);
+
+  _.each(api, ({ pathname, method, handler }) =>
+    router[method](pathname, handler)
+  );
+
+  app.server.baseRouter().mount("/ticket", router);
+
+  return {
+    async start() {},
+    async stop() {}
   };
 }
--- server/src/plugins/ticket/testTicket.js
+++ server/src/plugins/ticket/testTicket.js
@@ -1,20 +1,64 @@
-import assert from 'assert';
-import testMngr from '~/test/testManager';
+import assert from "assert";
+import testMngr from "~/test/testManager";
 
-describe('Ticket', function() {
+describe("Ticket No Auth", function() {
   let client;
   before(async () => {
-      await testMngr.start();
-      client = testMngr.client('alice');
-      assert(client);
-      //await client.login();
+    await testMngr.start();
+    client = testMngr.client("alice");
   });
   after(async () => {
-      await testMngr.stop();
+    await testMngr.stop();
   });
 
-  it('should get all tickets', async () => {
-    let tickets = await client.get('v1/ticket');
-    assert(tickets);
+  it("should get a 401 when getting all tickets", async () => {
+    try {
+      let tickets = await client.get("v1/ticket");
+      assert(tickets);
+    } catch (error) {
+      assert.equal(error.body, "Unauthorized");
+      assert.equal(error.statusCode, 401);
+    }
+  });
+  it("should get 403 when getting a ticket", async () => {
+    try {
+      let ticket = await client.get("v1/ticket/123456");
+      assert(ticket);
+    } catch (error) {
+      assert.equal(error.body, "Unauthorized");
+      assert.equal(error.statusCode, 401);
+    }
   });
 });
+
+
+describe("Ticket", function() {
+  let client;
+  before(async () => {
+    await testMngr.start();
+    client = testMngr.client("alice");
+    assert(client);
+    await client.login();
+  });
+  after(async () => {
+    await testMngr.stop();
+  });
+
+  it("should get all tickets", async () => {
+    let tickets = await client.get("v1/ticket");
+    assert(tickets);
+  });
+  it("should get 404 when the ticket is not found", async () => {
+    try {
+      let tickets = await client.get("v1/ticket/123456");
+      assert(tickets);
+    } catch (error) {
+      //console.log(error.body)
+      //console.log(error.statusCode)
+      assert(error.body.error);
+      assert.equal(error.body.error.name, "NotFound");
+      assert.equal(error.statusCode, 404);
+    }
+  });
+});
+

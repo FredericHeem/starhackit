2fd4b78b6bdc45cfd392d90acd3c99cadd881c55
Frederic Heem <frederic.heem@gmail.com>
2018-03-25 00:03:47 +0000

    /me uses new way to write rest api

--- server/src/plugins/users/me/MeRouter.js
+++ server/src/plugins/users/me/MeRouter.js
@@ -1,28 +1,56 @@
-import Router from 'koa-66';
-import MeApi from './MeApi';
 
-let log = require('logfilename')(__filename);
+export default function MeRouter(app /*auth*/) {
+  const { models } = app.data.sequelize;
+  const {validateSchema} = app.utils.api;
 
-export function MeHttpController(app){
-  log.debug("MeHttpController");
-  let respond = app.utils.http.respond;
-  let meApi = MeApi(app);
-  return {
-    async get(context) {
-      return respond(context, meApi, meApi.getByUserId, [context.state.user.id]);
-    },
-    async patch(context) {
-      return respond(context, meApi, meApi.patch, [context.state.user.id, context.request.body]);
+  const api = {
+    pathname: "/me",
+    middlewares: [app.server.auth.isAuthenticated],
+    ops: {
+      get: {
+        pathname: "/",
+        method: "get",
+        handler: async context => {
+          const user = await models.User.findByUserId(context.state.user.id);
+          context.body = user.get();
+          context.status = 200;
+        }
+      },
+      patch: {
+        pathname: "/",
+        method: "patch",
+        schema: require("./schema/patch.json"),
+        handler: async context => {
+          const userId = context.state.user.id;
+          const data = context.request.body;
+          if (!validateSchema(data, require("./schema/patch.json"), context))
+            return;
+          //TODO refactor with nested data
+          //TODO transaction ?
+          await models.User.update(data, {
+            where: {
+              id: userId
+            }
+          });
+          //TODO upsert ?
+          const profileData = {
+            biography: data.biography || ""
+          };
+
+          await models.Profile.update(profileData, {
+            where: {
+              user_id: userId
+            }
+          });
+          const updatedUser = await models.User.findByUserId(
+            context.state.user.id
+          );
+          context.body = updatedUser.get();
+          context.status = 200;
+        }
+      }
     }
   };
-}
 
-export default function MeRouter(app, /*auth*/){
-  let router = new Router();
-  let meHttpController = MeHttpController(app);
-  router.use(app.server.auth.isAuthenticated);
-  router.get('/', meHttpController.get);
-  router.patch('/', meHttpController.patch);
-  app.server.baseRouter().mount("/me", router);
-  return router;
+  app.server.createRouter(api);
 }

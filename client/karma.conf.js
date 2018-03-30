a5bf6f16d311e0975ec6b5f380f404a5f3d11adb
Frederic Heem <frederic.heem@gmail.com>
2018-03-30 01:01:45 +0100

    karma works again

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,38 +1,35 @@
 /*eslint-env node */
-var _ = require("lodash");
-var webpack = require('webpack');
-var path = require('path');
-var webpackConfig = require('./webpack.dev');
-webpackConfig.devtool = 'cheap-module-source-map';
+const _ = require("lodash");
+const webpack = require("webpack");
+const path = require("path");
+const webpackConfig = require("./webpack.dev");
+webpackConfig.devtool = "cheap-module-source-map";
 
-module.exports = function (config) {
-  webpackConfig.resolve.alias = _.omit(webpackConfig.resolve.alias, ['react', 'react-dom', 'create-react-class']);
-  var configuration = {
-    browsers: ['Chrome'], // ['Chrome'] run in Chrome, 'PhantomJS'
+module.exports = function(config) {
+  webpackConfig.resolve.alias = _.omit(webpackConfig.resolve.alias, [
+    "react",
+    "react-dom",
+    "create-react-class"
+  ]);
+  const configuration = {
+    browsers: ["Chrome"], // ['Chrome'] run in Chrome, 'PhantomJS'
     singleRun: true,
-     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'sinon'],
-    files: [
-      'src/**/*.spec.js'
-    ],
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ["mocha", "sinon"],
+    files: ["src/**/*.spec.js"],
     preprocessors: {
-      'src/**/*.js': ['webpack', 'sourcemap']
+      "src/**/*.js": ["webpack", "sourcemap"]
     },
-    reporters: ['mocha', 'coverage'],
+    reporters: ["mocha", "coverage"],
     webpack: {
       module: {
         rules: webpackConfig.module.rules
-       },
-       plugins: [
-         //new webpack.IgnorePlugin(/jsdom$/),
-         new webpack.DefinePlugin( {
-             __VERSION__: JSON.stringify('1.0')
-         } )
-      ],
+      },
+      plugins: webpackConfig.plugins,
       externals: {
-        'react/addons': true,
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true
+        "react/addons": true,
+        "react/lib/ExecutionEnvironment": true,
+        "react/lib/ReactContext": true
       },
       resolve: webpackConfig.resolve
     },
@@ -41,25 +38,25 @@ module.exports = function (config) {
     },
     coverageReporter: {
       reporters: [
-         {
-             type: 'text-summary'
-         },
-         {
-             type: 'html',
-             dir: 'coverage/'
-         }
-     ]
+        {
+          type: "text-summary"
+        },
+        {
+          type: "html",
+          dir: "coverage/"
+        }
+      ]
     },
     customLaunchers: {
-        Chrome_travis_ci: {
-            base: 'Chrome',
-            flags: ['--no-sandbox']
-        }
-    },
+      Chrome_travis_ci: {
+        base: "Chrome",
+        flags: ["--no-sandbox"]
+      }
+    }
   };
 
   if (process.env.TRAVIS) {
-      configuration.browsers = ['Chrome_travis_ci'];
+    configuration.browsers = ["Chrome_travis_ci"];
   }
-  config.set(configuration)
+  config.set(configuration);
 };

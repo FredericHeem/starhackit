0249e3e74dcdd21b3d866f0ccd0988fd90c76fc6
Frederic Heem <frederic.heem@gmail.com>
2018-04-17 18:43:18 +0100

    add test for push token

--- server/src/plugins/expoPush/testPushToken.js
+++ server/src/plugins/expoPush/testPushToken.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 import testMngr from "~/test/testManager";
+import Axios from "axios";
 
 describe("Candidate Job", function() {
   let client;
@@ -11,7 +12,7 @@ describe("Candidate Job", function() {
   after(async () => {
     await testMngr.stop();
   });
-  it.only("should get all token for the current user", async () => {
+  it("should get all token for the current user", async () => {
     const body = {
       token: "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]"
     };
@@ -19,5 +20,20 @@ describe("Candidate Job", function() {
     await client.post("v1/push_token/", body);
     await client.post("v1/push_token/", body);
   });
-  
+  it("send message", async () => {
+    const data = {
+      to: "ExponentPushToken[8giJthMgGEyYUr8H5NsPWO]",
+      title: "Someone apply",
+      body: "Someone apply to your job"
+    };
+    await Axios({
+      method: "post",
+      url: "https://exp.host/--/api/v2/push/send",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data
+    });
+
+  });
 });

75d23a7045d0bac37e8b2c81a916e2708c571e52
Frederic Heem <frederic.heem@gmail.com>
2019-09-01 15:02:46 +0100

    remove config.ts

--- server/src/config.ts
+++
@@ -1,7 +0,0 @@
-import { merge } from "lodash";
-import { config as brokenConfig } from "node-config-ts";
-
-const config = process.env.NODE_CONFIG
-  ? merge(brokenConfig, JSON.parse(process.env.NODE_CONFIG))
-  : brokenConfig;
-export default config;

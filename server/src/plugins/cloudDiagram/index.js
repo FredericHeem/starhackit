5e50235113ac687b25579e181bb3e9a687a4c4cf
Frederic Heem <frederic.heem@gmail.com>
2021-05-06 16:25:18 -0500

    move infra models to folder

--- server/src/plugins/cloudDiagram/index.js
+++ server/src/plugins/cloudDiagram/index.js
@@ -1,12 +1,11 @@
 const assert = require("assert");
-const { pipe, tap, tryCatch, map } = require("rubico");
 const { DiagramApi } = require("./diagramApi");
 
 const { InfraApi } = require("./infraApi");
 
 module.exports = (app) => {
-  app.data.registerModel(__dirname, `jobModel`);
-  app.data.registerModel(__dirname, `infraModel`);
+  app.data.registerModel(__dirname, `models/JobModel`);
+  app.data.registerModel(__dirname, `models/InfraModel`);
 
   DiagramApi(app);
   InfraApi(app);
